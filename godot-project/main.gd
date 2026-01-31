257b076933b9d354efd4741b6d3d4178654ae0b6
Harris <harris.ferguson@usask.ca>
2026-01-31 14:00:03 -0600

    added clique support (multi boid groups)

--- godot-project/main.gd
+++ godot-project/main.gd
@@ -1,23 +1,27 @@
 extends Node
 
 const BOIDS_COUNT = 20
+const CLIQUES_COUNT = 3
 const TARGET_DIRECTION = Vector3(1, 0, 0) 
 
 @onready var boid_scene = preload("res://scenes/boid.tscn")
-@onready var boids_container = $Boids
+@onready var clique_container = $Cliques
 
-var boids = []
+var cliques = []
 
 func _ready():
-	for i in BOIDS_COUNT:
-		var boid = boid_scene.instantiate()
-		boids_container.add_child(boid)
-		boids.push_back(boid)
-	
-	for boid in boids_container.get_children():
-		boid.boids = boids
-
+	for i in CLIQUES_COUNT:
+		var boids_container = Node3D.new()
+		var boids = []
+		for j in BOIDS_COUNT:
+			var boid = boid_scene.instantiate()
+			boids_container.add_child(boid)
+			boids.push_back(boid)
+		clique_container.add_child(boids_container)
+		for boid in boids_container.get_children():
+			boid.boids = boids
 
 func _process(delta):
-	for boid in boids_container.get_children():
-		boid.set_prey_position(TARGET_DIRECTION)
+	for clique in clique_container.get_children():
+		for boid in clique.get_children():
+			boid.set_prey_position(TARGET_DIRECTION)
