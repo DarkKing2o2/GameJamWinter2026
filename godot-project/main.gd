d7aa7b398fd96c45140346a9008e513cf4dce81b
Harris <harris.ferguson@usask.ca>
2026-01-31 14:23:37 -0600

    expand floor

--- godot-project/main.gd
+++ godot-project/main.gd
@@ -9,12 +9,21 @@ const TARGET_DIRECTION = Vector3(1, 0, 0)
 
 var cliques = []
 
+const CLIQUE_VALUES = [
+	{"cohesion": 0.5, "separation": 1.0, "centralization": 0.5},
+	{"cohesion": 0.8, "separation": 0.7, "centralization": 0.3},
+	{"cohesion": 0.3, "separation": 1.5, "centralization": 0.7}
+]
+
 func _ready():
 	for i in CLIQUES_COUNT:
 		var boids_container = Node3D.new()
 		var boids = []
 		for j in BOIDS_COUNT:
 			var boid = boid_scene.instantiate()
+			boid.cohesion_force = CLIQUE_VALUES[i]["cohesion"]
+			boid.seperation_force = CLIQUE_VALUES[i]["separation"]
+			boid.centralization_force = CLIQUE_VALUES[i]["centralization"]
 			boids_container.add_child(boid)
 			boids.push_back(boid)
 		clique_container.add_child(boids_container)
